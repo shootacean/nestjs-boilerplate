79115b136e298894e4286427ddd3496b51ab8180
Shota Narisaka <shootacean@gmail.com>
2022-07-09 23:50:15 +0900

    docs(openapi): /apiでOpen APIドキュメントを見れるようにする

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,20 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
